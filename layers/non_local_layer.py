bf3f055f49d4025c9e6ab92f8bbd452e18d2c4a5
Taekyu Lee <taekyulee@yonsei.ac.kr>
2020-09-07 11:17:38 +0900

    * Change non local related

---
+++ layers/non_local_layer.py
@@ -0,0 +1,19 @@
+from keras.layers import Layer
+import tensorflow as tf
+
+
+class NonLocal(Layer):
+    def __init__(self, units=32, input_dim=32):
+        super(NonLocal, self).__init__()
+        w_init = tf.random_normal_initializer()
+        self.w = tf.Variable(
+            initial_value=w_init(shape=(input_dim, units), dtype="float32"),
+            trainable=True,
+        )
+        b_init = tf.zeros_initializer()
+        self.b = tf.Variable(
+            initial_value=b_init(shape=(units,), dtype="float32"), trainable=True
+        )
+
+    def call(self, inputs):
+        return tf.matmul(inputs, self.w) + self.b
