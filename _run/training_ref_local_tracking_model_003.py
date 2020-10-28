14fa4df01b683f6268ef5bc2b00f90143c3fe047
Taekyu Lee <taekyulee@yonsei.ac.kr>
2020-10-28 14:22:21 +0900

    - Remove unused * Move Sequence generator

--- _run/training_ref_local_tracking_model_003.py
+++ _run/training_ref_local_tracking_model_003.py
@@ -1,43 +1,27 @@
-import math
 import os
 import sys
-
-from common_py.folder import files_in_folder
 
 sys.path.append(os.getcwd())
 
 import time
-from typing import Callable, Generator, List, Optional, Tuple
+from typing import List
 
 import common_py
-import numpy as np
-import toolz
 from common_py.dl.report import acc_loss_plot
+from common_py.folder import files_in_folder
 from image_keras.custom.callbacks_after_epoch import (
     EarlyStoppingAfter,
     ModelCheckpointAfter,
 )
-from image_keras.flow_directory import FlowFromDirectory, ImagesFromDirectory
-from image_keras.inout_generator import (
-    DistFlowManager,
-    DistInOutGenerator,
-    Distributor,
-    save_batch_transformed_img,
+from models.ref_local_tracking.ref_local_tracking_model_003.config import (
+    RefTrackingSequence,
 )
-from image_keras.utils.image_transform import img_to_ratio
 from tensorflow.keras.callbacks import Callback, History, TensorBoard
 
 if __name__ == "__main__":
     # Variables
     from models.ref_local_tracking.ref_local_tracking_model_003 import (
         RefModel003ModelHelper,
-        input_main_image_preprocessing_function,
-        input_ref_image_preprocessing_function,
-        input_ref_label_1_preprocessing_function,
-        input_ref_label_2_preprocessing_function,
-        input_ref_label_3_preprocessing_function,
-        input_ref_label_4_preprocessing_function,
-        output_label_preprocessing_function,
     )
 
     variable_training_dataset_folder = "ivan_filtered_training"
@@ -132,124 +116,7 @@ if __name__ == "__main__":
     val_batch_size: int = 4
     test_batch_size: int = 4
 
-    # 2.1 Input ---------
-    input_sizes = model_helper.model_descriptor.get_input_sizes()
-
-    # a) main image
-    import cv2
-    import tensorflow as tf
-
-    class RefTrackingSequence(tf.keras.utils.Sequence):
-        def __init__(
-            self,
-            image_file_names: List[str],
-            main_image_folder_name: str,
-            ref_image_folder_name: str,
-            ref_result_label_folder_name: str,
-            output_label_folder_name: str,
-            ref_result1_preprocessing_function: Callable[[np.ndarray], np.ndarray],
-            ref_result2_preprocessing_function: Callable[[np.ndarray], np.ndarray],
-            ref_result3_preprocessing_function: Callable[[np.ndarray], np.ndarray],
-            ref_result4_preprocessing_function: Callable[[np.ndarray], np.ndarray],
-            output_label_preprocessing_function: Callable[
-                [np.ndarray, np.ndarray], np.ndarray
-            ],
-            main_image_preprocessing_function: Optional[
-                Callable[[np.ndarray], np.ndarray]
-            ] = None,
-            ref_image_preprocessing_function: Optional[
-                Callable[[np.ndarray], np.ndarray]
-            ] = None,
-            batch_size: int = 1,
-            shuffle: bool = False,
-            seed: int = 42,
-        ):
-            self.image_file_names = image_file_names
-            self.main_image_folder_name = main_image_folder_name
-            self.ref_image_folder_name = ref_image_folder_name
-            self.ref_result_label_folder_name = ref_result_label_folder_name
-            self.output_label_folder_name = output_label_folder_name
-            self.main_image_preprocessing_function = main_image_preprocessing_function
-            self.ref_image_preprocessing_function = ref_image_preprocessing_function
-            self.ref_result1_preprocessing_function = ref_result1_preprocessing_function
-            self.ref_result2_preprocessing_function = ref_result2_preprocessing_function
-            self.ref_result3_preprocessing_function = ref_result3_preprocessing_function
-            self.ref_result4_preprocessing_function = ref_result4_preprocessing_function
-            self.output_label_preprocessing_function = (
-                output_label_preprocessing_function
-            )
-            self.batch_size = batch_size
-            self.shuffle = shuffle
-            self.seed = seed
-
-        def __len__(self):
-            return math.ceil(len(self.image_file_names) / self.batch_size)
-
-        def __getitem__(self, idx):
-            batch_image_file_names = self.image_file_names[
-                idx * self.batch_size : (idx + 1) * self.batch_size
-            ]
-
-            batch_main_images = []
-            batch_ref_images = []
-            batch_ref1_results = []
-            batch_ref2_results = []
-            batch_ref3_results = []
-            batch_ref4_results = []
-            batch_output_labels = []
-            for image_file_name in batch_image_file_names:
-                main_img = cv2.imread(
-                    os.path.join(self.main_image_folder_name, image_file_name),
-                    cv2.IMREAD_GRAYSCALE,
-                )
-                if self.main_image_preprocessing_function is not None:
-                    main_img = self.main_image_preprocessing_function(main_img)
-                batch_main_images.append(main_img)
-
-                ref_img = cv2.imread(
-                    os.path.join(self.ref_image_folder_name, image_file_name),
-                    cv2.IMREAD_GRAYSCALE,
-                )
-                if self.ref_image_preprocessing_function is not None:
-                    ref_img = self.ref_image_preprocessing_function(ref_img)
-                batch_ref_images.append(ref_img)
-
-                ref_result_label = cv2.imread(
-                    os.path.join(self.ref_result_label_folder_name, image_file_name)
-                )
-
-                ref_result1 = self.ref_result1_preprocessing_function(ref_result_label)
-                batch_ref1_results.append(ref_result1)
-
-                ref_result2 = self.ref_result2_preprocessing_function(ref_result_label)
-                batch_ref2_results.append(ref_result2)
-
-                ref_result3 = self.ref_result3_preprocessing_function(ref_result_label)
-                batch_ref3_results.append(ref_result3)
-
-                ref_result4 = self.ref_result4_preprocessing_function(ref_result_label)
-                batch_ref4_results.append(ref_result4)
-
-                output_label = cv2.imread(
-                    os.path.join(self.output_label_folder_name, image_file_name)
-                )
-                output_label = self.output_label_preprocessing_function(
-                    output_label, ref_result_label,
-                )
-                batch_output_labels.append(output_label)
-
-            X = [
-                np.array(batch_main_images),
-                np.array(batch_ref_images),
-                np.array(batch_ref1_results),
-                np.array(batch_ref2_results),
-                np.array(batch_ref3_results),
-                np.array(batch_ref4_results),
-            ]
-            Y = [np.array(batch_output_labels)]
-
-            return (X, Y)
-
+    # 2.1 Training, Validation Generator ---------
     training_images_file_names = files_in_folder(training_main_image_folder)
     training_samples = len(training_images_file_names)
     training_sequence = RefTrackingSequence(
@@ -258,17 +125,6 @@ if __name__ == "__main__":
         ref_image_folder_name=training_ref_image_folder,
         ref_result_label_folder_name=training_ref_result_label_folder,
         output_label_folder_name=training_output_main_label_folder,
-        main_image_preprocessing_function=toolz.compose_left(
-            input_main_image_preprocessing_function, img_to_ratio
-        ),
-        ref_image_preprocessing_function=toolz.compose_left(
-            input_ref_image_preprocessing_function, img_to_ratio
-        ),
-        ref_result1_preprocessing_function=input_ref_label_1_preprocessing_function,
-        ref_result2_preprocessing_function=input_ref_label_2_preprocessing_function,
-        ref_result3_preprocessing_function=input_ref_label_3_preprocessing_function,
-        ref_result4_preprocessing_function=input_ref_label_4_preprocessing_function,
-        output_label_preprocessing_function=output_label_preprocessing_function,
         batch_size=training_batch_size,
         shuffle=True,
     )
@@ -281,17 +137,6 @@ if __name__ == "__main__":
         ref_image_folder_name=val_ref_image_folder,
         ref_result_label_folder_name=val_ref_result_label_folder,
         output_label_folder_name=val_output_main_label_folder,
-        main_image_preprocessing_function=toolz.compose_left(
-            input_main_image_preprocessing_function, img_to_ratio
-        ),
-        ref_image_preprocessing_function=toolz.compose_left(
-            input_ref_image_preprocessing_function, img_to_ratio
-        ),
-        ref_result1_preprocessing_function=input_ref_label_1_preprocessing_function,
-        ref_result2_preprocessing_function=input_ref_label_2_preprocessing_function,
-        ref_result3_preprocessing_function=input_ref_label_3_preprocessing_function,
-        ref_result4_preprocessing_function=input_ref_label_4_preprocessing_function,
-        output_label_preprocessing_function=output_label_preprocessing_function,
         batch_size=val_batch_size,
         shuffle=False,
     )
@@ -319,7 +164,6 @@ if __name__ == "__main__":
             + "_{"
             + val_checkpoint_metric
             + ":.3f}.hdf5",
-            # + ".epoch_{epoch:02d}-val_loss_{val_loss:.3f}-val_mean_iou_{val_mean_iou:.3f}.hdf5",
         ),
         verbose=1,
         after_epoch=apply_callbacks_after,
