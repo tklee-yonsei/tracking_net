48e1bbec896e927284d3f40ac8849a7187626971
Taekyu Lee <taekyulee@yonsei.ac.kr>
2021-01-12 17:21:53 +0900

    * Change assert as tf's + Add extract patch shape test

--- tests/test_layers/test_dense_layer.py
+++ tests/test_layers/test_dense_layer.py
@@ -9,14 +9,11 @@ class DenseLayerTest(tf.test.TestCase):
         self.my_dense.build(input_shape=(2, 2))
 
     def testDenseLayerOutput(self):
-        self.my_dense.set_weights([np.array([[1, 0], [2, 3]]), np.array([0.5, 0])])
-        input_x = np.array([[1, 2], [2, 3]])
+        self.my_dense.set_weights([tf.constant([[1, 0], [2, 3]]), np.array([0.5, 0])])
+        input_x = tf.constant([[1, 2], [2, 3]])
         output = self.my_dense(input_x)
 
-        expected_output = np.array([[5.5, 6.0], [8.5, 9]])
+        expected_output = tf.constant([[5.5, 6.0], [8.5, 9]])
 
         # assertion
-        self.assertAllEqual(expected_output, output)
-
-
-# tf.test.main()
+        tf.debugging.assert_equal(expected_output, output)
---
+++ tests/test_layers/test_extract_patch_layer.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+from layers.extract_patch_layer import ExtractPatchLayer
+
+
+class ExtractPatchLayerTest(tf.test.TestCase):
+    def testExtractPatchLayerShapes(self):
+        batch_size = 2
+        hw_size = 5
+        channel_size = 3
+        k_size = 3
+
+        input_feature = tf.ones([batch_size, hw_size, hw_size, channel_size])
+        output = ExtractPatchLayer(k_size=k_size)(input_feature)
+
+        # assertion
+        tf.debugging.assert_shapes(
+            [(output, (batch_size, hw_size, hw_size, k_size * k_size * channel_size))]
+        )
+
